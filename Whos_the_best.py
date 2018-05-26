def4ebb11ea120ddb559cc719501b24b5f6c8f48
briandchristman <39653350+briandchristman@users.noreply.github.com>
2018-05-26 09:26:21 -0700

    First attempt at strings and input

---
+++ Whos_the_best.py
@@ -0,0 +1,21 @@
+# setting ititial variables
+oldstring = "Brian is the best!"
+addstring = "Hey! Hey! Hey! Hey!"
+
+# declare variable newstring initialize
+newstring1 = oldstring.replace('best','coolest')
+newstring2 = newstring1.replace('coolest', 'greatest')
+
+
+
+#Validation
+print (oldstring.upper())
+print(newstring1.upper())
+print(newstring2.upper())
+print(addstring.upper())
+
+
+
+name = raw_input("What is your name? ")
+quest = raw_input("What is your quest? ")
+color = raw_input("What is your favorite color? ")
