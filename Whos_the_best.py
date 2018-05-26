f2025929c460eae185189a4912594bd7ff9c4baf
briandchristman <39653350+briandchristman@users.noreply.github.com>
2018-05-26 13:20:11 -0700

    ask for adjectives

--- Whos_the_best.py
+++ Whos_the_best.py
@@ -1,14 +1,15 @@
 name = input("What is your name? ") # Get name.
-
-
+adj1 = input("Enter an adjective:") # get first descriptor
+adj2 = input("Enter an adjective:") # get second descriptor
+adj3 = input("Enter an adjective:") # get third descriptor
 
 # setting ititial variables
-oldstring = "%s is the best!" % name
+oldstring = "%s is the %s!" % (name, adj1)
 addstring = "Hey! Hey! Hey! Hey!"
 
 # declare variable newstring initialize
-newstring1 = oldstring.replace('best','coolest')
-newstring2 = newstring1.replace('coolest', 'greatest')
+newstring1 = oldstring.replace(adj1,adj2)
+newstring2 = newstring1.replace(adj2, adj3)
 
 
 
